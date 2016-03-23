baea7b4025d0b940ce9e2410c30a8934f59821d1
Neal Friedman <nealiof1000@gmail.com>
2016-03-22 22:00:16 -0600

    formatting

--- src/Floyd.java
+++ src/Floyd.java
@@ -1,4 +1,3 @@
-
 import java.util.Scanner;
 
 /**
@@ -28,7 +27,6 @@ public class Floyd {
 //        { -1, -1,  0,  4, -1},
 //        { -1, -1,  2,  0,  3},
 //        { 3,  -1, -1, -1,  0}};
-
 
     public static void main(String[] args)
     {
