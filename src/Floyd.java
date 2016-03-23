b5ff557bec3e15353d0502fa69a24082660160d0
Neal Friedman <nealiof1000@gmail.com>
2016-03-22 21:48:48 -0600

    cleaned up formatting. globalized variables. enables user console input instead of hardcoded table

--- src/Floyd.java
+++ src/Floyd.java
@@ -12,27 +12,43 @@ import java.util.Scanner;
  */
 public class Floyd {
 
+    private static int[][] distanceTable;
+    private static int[][] pathTable;
     private static int numOfVertices;
-    private static int distanceTable[][];
-    private static int distanceTableHardCode[][] = {
-            { 0,  1, -1,  1,  5},
-            { 9,  0,  3,  2, -1},
-            { -1, -1,  0,  4, -1},
-            { -1, -1,  2,  0,  3},
-            { 3,  -1, -1, -1,  0}};
+
+    // Hardcoded table for testing
+//    private static int [][] distanceTable = {
+//        { 0,  1, -1,  1,  5},
+//        { 9,  0,  3,  2, -1},
+//        { -1, -1,  0,  4, -1},
+//        { -1, -1,  2,  0,  3},
+//        { 3,  -1, -1, -1,  0}};
+
 
     public static void main(String[] args)
     {
+        // Wait for user input
+        // TODO make sure user input is correct
         System.out.print("Enter n for number of vertices: ");
         Scanner sysin = new Scanner( System.in );
         numOfVertices = sysin.nextInt();
         distanceTable = new int[numOfVertices][numOfVertices];
+        pathTable = new int[numOfVertices][numOfVertices];
 
+        for(int i = 0; i < numOfVertices; i++){
+            System.out.println("Enter row weights seperated by space: ");
+                for(int j = 0; j < numOfVertices; j++)
+                        distanceTable[i][j] = sysin.nextInt();
+        }
+
+        floydsAlgorithm();
+    }
+
+    private static void floydsAlgorithm(){
         // Print initial table
-        System.out.println("\n---------------- D(0) ----------------");
-        print(distanceTableHardCode);
-        System.out.println("--------------------------------------");
-
+        System.out.println("\n-------------------------- D(0) --------------------------");
+        print(distanceTable, pathTable);
+        System.out.println("----------------------------------------------------------");
 
         // Floyds Algorithm
         // k = iteration number
@@ -41,33 +57,31 @@ public class Floyd {
         for(int k = 0; k < numOfVertices; k++) {
             for (int i = 0; i < numOfVertices; i++) {
                 for (int j = 0; j < numOfVertices; j++) {
-                    if ((distanceTableHardCode[i][j] > (distanceTableHardCode[i][k] + distanceTableHardCode[k][j])
-                            || distanceTableHardCode[i][j] < 0)
-                            && distanceTableHardCode[k][j] > 0
-                            && distanceTableHardCode[i][k] > 0)
-                        distanceTableHardCode[i][j] = distanceTableHardCode[i][k] + distanceTableHardCode[k][j];
+                    if ((distanceTable[i][j] > (distanceTable[i][k] + distanceTable[k][j])
+                            || distanceTable[i][j] < 0)
+                            && distanceTable[k][j] > 0
+                            && distanceTable[i][k] > 0) {
+                        distanceTable[i][j] = distanceTable[i][k] + distanceTable[k][j];
+                        pathTable[i][j] = k+1;
+                    }
                 }
             }
-            System.out.println("\n---------------- D(" + (k+1) + ") ----------------");
-            print(distanceTableHardCode);
-            System.out.println("--------------------------------------");
+            System.out.println("\n-------------------------- D(" + (k+1) + ") --------------------------");
+            print(distanceTable, pathTable);
+            System.out.println("----------------------------------------------------------");
         }
     }
-
-    private static void printRow(int[] row) {
-        for (int i : row) {
-            if(i == -1)
-                System.out.print("-");
-            else
-                System.out.print(i);
-            System.out.print("\t||\t");
-        }
-        System.out.println();
-    }
-
-    private static void print(int[][] table){
-        for(int[] row : table) {
-            printRow(row);
+    
+    private static void print(int[][] table1, int[][] table2){
+        for(int i = 0; i < table1.length && i < table2.length; i++){
+            for(int k = 0; k < table1[i].length && k < table2[i].length; k++){
+                if(table1[i][k] == -1)
+                    System.out.print("-" + " (" + table2[i][k] + ")");
+                else
+                    System.out.print(table1[i][k] + " (" + table2[i][k] + ")");
+                System.out.print("\t||\t");
+            }
+            System.out.println();
         }
     }
 }
